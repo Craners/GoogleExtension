49a5b805f5778b8b571a549cc3339d5378cb145f
Amir Kiumarsi <codacy20@users.noreply.github.com>
2018-03-05 21:24:07 +0100

    setup for world clock and click function for switching between clocks.

--- extension/worldClock.js
+++ extension/worldClock.js
@@ -1,26 +1,35 @@
 // var x3 = moment.tz.names(); // ["Africa/Abidjan", "Africa/Accra", "Africa/Addis_Ababa", ...]
 var listOfTimes=[];
-var def = moment.tz.guess();
-listOfTimes.push(def);
+var def = moment.tz.guess(); //guess where they are?
 var m = moment();
-m.format();// 2013-11-18T11:55:00-05:00
+m.format();
 listOfTimes.push(m);
-var x = m.tz("Europe/Berlin").format(); // 2013-11-18T06:00:00+01:00
-x = moment(x,"Y-MM-DD HH:mm");   // parsed as 4:30 local time
+var x1 = m.tz(def).format(); // 2013-11-18T06:00:00+01:00
+x1 = moment(x1);   // parsed as 4:30 local time
+listOfTimes.push(x1);
 var x2 = m.tz("America/Toronto").format(); // 2013-11-18T06:00:00+01:00
-x2 = moment(x2,"+-HH:mm");   // parsed as 4:30 local time
-// newYork.format();// 2014-06-01T12:00:00-04:00
+x2 = moment(x2);   // parsed as 4:30 local time
+listOfTimes.push(x2);
 $("#worldClockTime").html(x2._i);
-$('#worldClockTime').attr("data-index", "bar");
+$('#worldClockTime').attr("data-index", listOfTimes.indexOf(x2));
 console.log(listOfTimes);
 
 
 function ShowWorldClock(){
 
-//  console.log($("#worldClockTime"));   
+  var current  = parseInt($('#worldClockTime').attr("data-index"));
+  var rand = 0;
+  if(current == listOfTimes.length-1){
+
+    rand = 1;
+  }else{
+
+    rand = current+1
+  }
+  $("#worldClockTime").html(listOfTimes[rand]._i);
+  $('#worldClockTime').attr("data-index", rand);
 }
 
 $( "#worldClockTime" ).click(function() {
-    alert( "Handler for .click() called." );
     ShowWorldClock();
   });
