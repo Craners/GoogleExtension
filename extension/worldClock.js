dc76637118f405877aeaae0042803593e4a8aa65
Codacy20 <kmuc72@gmail.com>
2018-03-11 13:05:18 +0100

    added world clock autoComplete

--- extension/worldClock.js
+++ extension/worldClock.js
@@ -1,35 +1,61 @@
-// var x3 = moment.tz.names(); // ["Africa/Abidjan", "Africa/Accra", "Africa/Addis_Ababa", ...]
-var listOfTimes=[];
-var def = moment.tz.guess(); //guess where they are?
-var m = moment();
-m.format();
-listOfTimes.push(m);
-var x1 = m.tz(def).format(); // 2013-11-18T06:00:00+01:00
-x1 = moment(x1);   // parsed as 4:30 local time
-listOfTimes.push(x1);
-var x2 = m.tz("America/Toronto").format(); // 2013-11-18T06:00:00+01:00
-x2 = moment(x2);   // parsed as 4:30 local time
-listOfTimes.push(x2);
-$("#worldClockTime").html(x2._i);
-$('#worldClockTime').attr("data-index", listOfTimes.indexOf(x2));
-console.log(listOfTimes);
+$(document).ready(function () {
 
 
-function ShowWorldClock(){
+  var listOfTimes = [];
+  var def = moment.tz.guess(); //guess where they are?
+  var m = moment();
+  m.format();
+  listOfTimes.push(m);
 
-  var current  = parseInt($('#worldClockTime').attr("data-index"));
-  var rand = 0;
-  if(current == listOfTimes.length-1){
-
-    rand = 1;
-  }else{
-
-    rand = current+1
+  if (typeof (Storage) !== "undefined") {
+    if (localStorage.getItem("clocks")) {
+      var name = 'America/Toronto';
+      var x1 = m.tz(name).format(); // 2013-11-18T06:00:00+01:00
+      x1 = moment(x1);   // parsed as 4:30 local time
+      listOfTimes.push(x1);
+    } else {
+      var x1 = m.tz(def).format(); // 2013-11-18T06:00:00+01:00
+      x1 = moment(x1);   // parsed as 4:30 local time
+      listOfTimes.push(x1);
+      var x2 = m.tz("America/Toronto").format(); // 2013-11-18T06:00:00+01:00
+      x2 = moment(x2);   // parsed as 4:30 local time
+      listOfTimes.push(x2);
+    }
+  } else {
+    console.log('local storage not supported');
   }
-  $("#worldClockTime").html(listOfTimes[rand]._i);
-  $('#worldClockTime').attr("data-index", rand);
-}
 
-$( "#worldClockTime" ).click(function() {
+  $("#worldClockTime").html(x2._i);
+  $('#worldClockTime').attr("data-index", listOfTimes.indexOf(x2));
+  // console.log(x3);
+
+
+  function ShowWorldClock() {
+
+    var current = parseInt($('#worldClockTime').attr("data-index"));
+    var rand = 0;
+    if (current == listOfTimes.length - 1) {
+
+      rand = 1;
+    } else {
+
+      rand = current + 1
+    }
+    $("#worldClockTime").html(listOfTimes[rand]._i);
+    $('#worldClockTime').attr("data-index", rand);
+  }
+
+  $("#worldClockTime").click(function () {
     ShowWorldClock();
   });
+
+  $("#addCity").autocomplete({
+    source: moment.tz.names(),
+    minLength: 2,
+    select: function (event, ui) {
+      console.log(ui.item ?
+        "Selected: " + ui.item.value + " aka " + ui.item.id :
+        "Nothing selected, input was " + this.value);
+    }
+  });
+});
