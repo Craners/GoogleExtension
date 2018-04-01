c8beda5b2d3c9d8d36bbdc66b94fb317cf20286b
Codacy20 <kmuc72@gmail.com>
2018-04-01 12:09:23 +0200

    the new styling for clocks and right alignment

--- extension/worldClock.js
+++ extension/worldClock.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
   var listOfTimes = [];
-  var InitlistOfTimes =[];
+  var InitlistOfTimes = [];
   var dateFormat = 'ddd, MMM D, H:mm'; //<-------- This part will get rid of the warning.
   init();
 
   function init() {
 
     listOfTimes = [];
-    InitlistOfTimes =[];
+    InitlistOfTimes = [];
 
     var def = moment.tz.guess(); //guess where they are?
     var m = moment();
@@ -16,7 +16,7 @@ $(document).ready(function () {
     listOfTimes.push(m);
 
     if (typeof (Storage) !== "undefined") {
-      if (localStorage.getItem("data") && GetLocal('WorldClock')!=='') {
+      if (localStorage.getItem("data") && GetLocal('WorldClock') !== '') {
 
         InitlistOfTimes = GetLocal('WorldClock');
         InitlistOfTimes.forEach(element => {
@@ -34,7 +34,7 @@ $(document).ready(function () {
         listOfTimes.push(x1);
         SaveLocal('WorldClock', def);
 
-        InitlistOfTimes.push("America/Toronto");        
+        InitlistOfTimes.push("America/Toronto");
         var x2 = m.tz("America/Toronto").format(dateFormat); // 2013-11-18T06:00:00+01:00
         x2 = moment(x2);   // parsed as 4:30 local time
         listOfTimes.push(x2);
@@ -45,15 +45,21 @@ $(document).ready(function () {
 
     let tempCity = listOfTimes[listOfTimes.length - 1];
     let tempCityName = InitlistOfTimes[InitlistOfTimes.length - 1];
-    $("#worldClockTime").html(tempCity._i);
-    $('#worldClockTime').attr("data-index", listOfTimes.indexOf(tempCity));
-    $('#cityInitial').html(tempCityName.substring(tempCityName.indexOf("/") + 1,tempCityName.indexOf("/") + 4));
+
+    var table = document.getElementById("clockTable");
+    var row = table.insertRow(0);
+    row.id = "rowClock";
+    $('#rowClock').attr("data-index", listOfTimes.indexOf(tempCity));
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = tempCityName.substring(tempCityName.indexOf("/") + 1);
+    cell2.innerHTML = tempCity._i;
   }
 
 
   function ShowWorldClock() {
 
-    var current = parseInt($('#worldClockTime').attr("data-index"));
+    var current = parseInt($('#rowClock').attr("data-index"));
     var rand = 0;
     if (current == listOfTimes.length - 1) {
 
@@ -62,12 +68,19 @@ $(document).ready(function () {
 
       rand = current + 1
     }
-    $("#worldClockTime").html(listOfTimes[rand]._i);
-    $('#worldClockTime').attr("data-index", rand);
-    $('#cityInitial').html(InitlistOfTimes[rand-1].substring(InitlistOfTimes[rand-1].indexOf("/") + 1,InitlistOfTimes[rand-1].indexOf("/") + 4));
+
+    var table = document.getElementById("clockTable");
+    $('#rowClock').remove();
+    var row = table.insertRow(0);
+    row.id = "rowClock";
+    $('#rowClock').attr("data-index", rand);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = InitlistOfTimes[rand - 1].substring(InitlistOfTimes[rand - 1].indexOf("/") + 1);
+    cell2.innerHTML = listOfTimes[rand]._i;
   }
 
-  $("#worldClockTime").click(function () {
+  $("#clockTable").click(function () {
     ShowWorldClock();
   });
 
@@ -81,3 +94,4 @@ $(document).ready(function () {
     }
   });
 });
+
