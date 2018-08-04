ccc334c56b230f92cfefd9312d385a5b27635ccd
amir <kmuc72@gmail.com>
2018-08-04 19:27:39 +1000

    image server is now up

--- extension/background.js
+++ extension/background.js
@@ -1,39 +1,6 @@
-$(document).ready(function () {
-
-  if (GetLocal("BackGround") === "") {
-
-    var check = moment(moment(), 'YYYY/MM/DD');
-    var day = check.format('D');
-    var rand = Math.floor((Math.random() * 8) + 1);
-
-    var obj = {
-
-      "today": day,
-      "randomNumber": rand
-    }
-    SaveOneOnly('BackGround', obj);
-    $('.layout-transparent').css('background-image', 'url(' + `../background/${rand}.jpg` + ')');
-  } else {
-
-    var data = GetLocal("BackGround");
-    var x = data.length-1;
-    data = data[x];
-
-    var check = moment(moment(), 'YYYY/MM/DD');
-    var day = check.format('D');
-
-    if (data.today == day) {
-
-      $('.layout-transparent').css('background-image', 'url(' + `../background/${data.randomNumber}.jpg` + ')');
-    }
-    else {
-
-      var rand = Math.floor((Math.random() * 8) + 1);
-      data.today = day;
-      data.randomNumber = rand;
-
-      SaveOneOnly('BackGround', data);
-      $('.layout-transparent').css('background-image', 'url(' + `../background/${data.randomNumber}.jpg` + ')');
-    }
-  }
-});
+// $(document).ready(function () {
+//   $('.layout-transparent').css('background-image', 'url(' + 'https://amirk.herokuapp.com/images/1.jpg' + ')');
+//   $('.layout-transparent').fadeIn("slow");
+// });
+$('.layout-transparent').css('background-image', 'url(' + 'https://amirk.herokuapp.com/images/back.jpg' + ')');
+$('.layout-transparent').animate({ opacity: 1 }, { duration: 'slow' });
